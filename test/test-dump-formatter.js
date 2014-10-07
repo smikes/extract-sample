4699cde9c9c9b81cad40bdb8547ed40ed452a509
Sam Mikes <smikes@cubane.com>
2014-10-07 14:11:35 +0100

    factor out fixture

--- test/test-dump-formatter.js
+++ test/test-dump-formatter.js
@@ -10,23 +10,30 @@ it('imports the module', function () {
 
 describe('formatted dump', function () {
     // suitable for inclusion in a C/C++ file
-    // example format:
-    // Extracted from <file> (<modified date>)
-    // nn bytes from 0x00001234 to 0x00004567
-    // const char * file_offset_length = \ /* 0x00001234 */
+
     // "$ 00 01 02 03 04 05 06 07 08  09 0A 0B 0C 0D 0E 0F ................." \ // 00-0F
     // "$ 00 01 02 03 04 05 06 07 08  09 0A 0B 0C 0D 0E 0F ................." \ // 10-1F
     // ;
+    var fixture = {
+        filename: 'cat.exe',
+        mtime: new Date('Mon 10 Oct 2011 23:24:11 GMT'),
+        offset: 1234,
+        buffer: new Buffer(37)
+    };
 
+    // Extracted from <file> (<modified date>)
     it('formats a header line', function () {
-        assert.equal(dumpFormatter.headerLine({filename: 'cat.exe',
-                                               mtime: new Date('Mon 10 Oct 2011 23:24:11 GMT')}),
+        assert.equal(dumpFormatter.headerLine(fixture),
                      "// Extracted from cat.exe [Mon, 10 Oct 2011 23:24:11 GMT]");
     });
 
+    // nn bytes from 0x00001234 to 0x00004567
     it('formats a second header line', function () {
-        assert.equal(dumpFormatter.headerLine2({offset: 1234,
-                                                buffer: new Buffer(37)}),
+        assert.equal(dumpFormatter.headerLine2(fixture),
                      "// 37 bytes from 0x000004D2 to 0x000004F7");
+    });
+
+    // const char * file_offset_length = \ /* 0x00001234 */
+    it('formats a variable declaration', function () {
     });
 });
