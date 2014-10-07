3a27f63507a29ec424b73b697aa228ea14969af5
Sam Mikes <smikes@cubane.com>
2014-10-07 13:41:19 +0100

    initial testing setup

---
+++ test/simple.js
@@ -0,0 +1,8 @@
+/*global it*/
+'use strict';
+
+var assert = require('assert');
+
+it('has a unit test', function () {
+    assert.equal(1 + 1, 2);
+});
---
+++ test/test-dump-formatter.js
@@ -0,0 +1,9 @@
+/*global describe, it*/
+'use strict';
+
+var assert = require('assert');
+var dumpFormatter = require('../lib/dump-formatter');
+
+it('imports the module', function () {
+    assert.ok(dumpFormatter);
+});
